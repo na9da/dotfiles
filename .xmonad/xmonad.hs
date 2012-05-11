ea630be0e4b9e6c86ac64dcb26559e50d9fd5f82
nanda@mobme.in <nanda@mir.localdomain>
2012-05-11 13:09:38 +0530

    Adding key bindings for sleep, suspend & lock

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,12 +1,20 @@
 import XMonad
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Util.EZConfig
 
-main = do
-  xmonad $ defaultConfig
-    { terminal    = myTerminal
-    , modMask     = myModMask
-    , borderWidth = myBorderWidth
-    }
 
-myTerminal    = "urxvt"
-myModMask     = mod4Mask
-myBorderWidth = 1
+main = xmonad $ defaultConfig { 
+      terminal    = "urxvt"
+    , modMask     = mod4Mask
+    , borderWidth = 1
+    , handleEventHook = fullscreenEventHook
+    } 
+    `additionalKeysP` myKeys
+
+
+myKeys = 
+  [ ("<XF86Sleep>", spawn "slock && dbus-send --system --print-reply --dest='org.freedesktop.UPower' /org/freedesktop/UPower org.freedesktop.UPower.Suspend")
+  , ("<XF86Suspend>", spawn "slock && dbus-send --system --print-reply --dest='org.freedesktop.UPower' /org/freedesktop/UPower org.freedesktop.UPower.Hibernate")
+  , ("<XF86ScreenSaver>", spawn "slock")
+  ]
+
